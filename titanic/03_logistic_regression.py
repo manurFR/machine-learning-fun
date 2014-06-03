add047c32c3e5040f4670da2f2ac2e22de8334a5
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2014-06-03 17:27:54 +0200

    added a learning curve to evaluate bias/variance of logistic regression applied to the problem

--- titanic/03_logistic_regression.py
+++ titanic/03_logistic_regression.py
@@ -6,7 +6,8 @@ import numpy as np
 import scipy as sp
 import pandas as pan
 import sklearn.linear_model, sklearn.preprocessing
-from sklearn.cross_validation import KFold
+from sklearn.cross_validation import KFold, train_test_split
+from matplotlib import pylab
 
 def prepareFeatures(dataframe, median_ages=None):
 	X = dataframe[['Pclass', 'Fare', 'Age']]
@@ -72,6 +73,33 @@ for C in [0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0]:
 		best_C = C
 
 print "Best C =", best_C
+
+datasizes = np.arange(50, len(X), 20)
+
+train_errors = []
+cv_errors = []
+for datasize in datasizes:
+	X_train, X_cv, y_train, y_cv = train_test_split(X[:datasize], Y[:datasize], test_size=0.7)
+	lr = sklearn.linear_model.LogisticRegression(C=best_C)
+	lr.fit(X_train,y_train)
+	train_errors.append(1 - lr.score(X_train, y_train))
+	cv_errors.append(1 - lr.score(X_cv, y_cv))
+
+def plot_bias_variance(data_sizes, train_errors, test_errors, title):
+    pylab.figure(num=None, figsize=(6, 5))
+    pylab.ylim([0.0, 1.0])
+    pylab.xlabel('Data set size')
+    pylab.ylabel('Error')
+    pylab.title(title)
+    pylab.plot(
+        data_sizes, test_errors, "--", data_sizes, train_errors, "b-", lw=1)
+    pylab.legend(["train error", "test error"], loc="upper right")
+    pylab.grid(True, linestyle='-', color='0.75')
+    pylab.show()
+
+plot_bias_variance(datasizes, train_errors, cv_errors, "Learning curve for Titanic Logistic Regression")
+# the curve shows we have high bias (we don't really fit the training set much better than the cross-validation set)
+#  ==> we need a more complex model (non-linear?)
 
 lr = sklearn.linear_model.LogisticRegression(C = best_C)
 lr.fit(X,Y)
