fe6c9a5bfcf4fae18a3976efe6023a5ddd156cf9
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2014-06-06 00:19:08 +0200

    factorize test_algo and add_title in utils

--- titanic/05_bias_variance_for_forest.py
+++ titanic/05_bias_variance_for_forest.py
@@ -2,13 +2,10 @@
 # -*- coding: UTF8 -*-
 
 import numpy as np
-import pandas as pan
-import re
-from utils import load_train_data, plot_bias_variance, output_predictions, add_sex_bit, fill_fare
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from utils import load_train_data, plot_bias_variance, output_predictions, add_sex_bit, fill_fare, add_title, test_algo
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.cross_validation import KFold, train_test_split
+from sklearn.cross_validation import train_test_split
 
 df, X, Y = load_train_data()
 
@@ -28,35 +25,6 @@ for datasize in datasizes:
 
 #plot_bias_variance(datasizes, train_errors, cv_errors, "Learning curve (Bias/Variance detection) for Random Forest")	
 # A small gap between train errors and cv errors stays even for large data size : maybe a bit of overfitting
-
-def add_title(X, names=None):
-	pattern = re.compile(r'.*(Mr|Mrs|Miss|Rev|Major|Col|Lady|Master|Dr).*')
-	def extract_title(name):
-		match = pattern.match(name)
-		if match:
-			return match.group(1)
-		else:
-			return 'Other'
-	if names is None:
-		names = X['Name']
-	title_encoder = LabelEncoder()
-	X['Title'] = title_encoder.fit_transform(names.apply(extract_title))
-
-def test_algo(algo, features, classes, name, options={}):
-	cv = KFold(n=len(features), n_folds=8, indices=True)
-
-	scores = []
-	for train, test in cv:
-		X_train, y_train = features.values[train], classes.values[train]
-		X_test, y_test = features.values[test], classes.values[test]
-
-		classifier = algo(**options)
-		classifier.fit(X_train, y_train)
-
-		scores.append(classifier.score(X_test, y_test))
-
-	score = np.mean(scores)
-	print "Score on training set (with cross-validation) for %s : %.4f" % (name, score)
 
 add_title(X, names)
 X = X.drop(['AgeFill'], axis=1)
--- titanic/utils.py
+++ titanic/utils.py
@@ -3,7 +3,10 @@
 
 import pandas as pan
 import numpy as np
+import re
 from matplotlib import pylab
+from sklearn.cross_validation import KFold
+from sklearn.preprocessing import LabelEncoder
 
 def add_sex_bit(X):
 	X['SexBit'] = X['Sex'].map({'female': 0, 'male':1}).astype(int)
@@ -22,6 +25,19 @@ def fill_median_age(X):
 		for j in range(3):
 			X.loc[(X.Age.isnull()) & (X.SexBit == i) & (X.Pclass-1 == j), 'AgeFill'] = median_ages[i,j]
 
+def add_title(X, names=None):
+	pattern = re.compile(r'.*(Mr|Mrs|Miss|Rev|Major|Col|Lady|Master|Dr).*')
+	def extract_title(name):
+		match = pattern.match(name)
+		if match:
+			return match.group(1)
+		else:
+			return 'Other'
+	if names is None:
+		names = X['Name']
+	title_encoder = LabelEncoder()
+	X['Title'] = title_encoder.fit_transform(names.apply(extract_title))
+
 def load_train_data():
 	#PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 	df = pan.read_csv('train.csv', header=0)
@@ -34,6 +50,22 @@ def load_train_data():
 	fill_median_age(X)
 
 	return df, X, Y
+
+def test_algo(algo, features, classes, name, options={}):
+	cv = KFold(n=len(features), n_folds=8, indices=True)
+
+	scores = []
+	for train, test in cv:
+		X_train, y_train = features.values[train], classes.values[train]
+		X_test, y_test = features.values[test], classes.values[test]
+
+		classifier = algo(**options)
+		classifier.fit(X_train, y_train)
+
+		scores.append(classifier.score(X_test, y_test))
+
+	score = np.mean(scores)
+	print "Score on training set (with cross-validation) for %s : %.4f" % (name, score)
 
 def plot_bias_variance(data_sizes, train_errors, test_errors, title):
     pylab.figure(num=None, figsize=(6, 5))
