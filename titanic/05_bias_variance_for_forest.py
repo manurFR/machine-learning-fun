46b8cbebe4d112702d414064aaf6686e0651fdcd
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2014-06-05 11:54:21 +0200

    move plot_bias_variance method to utils

--- titanic/05_bias_variance_for_forest.py
+++ titanic/05_bias_variance_for_forest.py
@@ -3,24 +3,12 @@
 
 import numpy as np
 import pandas as pan
-from matplotlib import pylab
-from utils import load_train_data
+
+from utils import load_train_data, plot_bias_variance
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
-
-def plot_bias_variance(data_sizes, train_errors, test_errors, title):
-    pylab.figure(num=None, figsize=(6, 5))
-    pylab.ylim([0.0, 1.0])
-    pylab.xlabel('Data set size')
-    pylab.ylabel('Error')
-    pylab.title(title)
-    pylab.plot(
-        data_sizes, test_errors, "--", data_sizes, train_errors, "b-", lw=1)
-    pylab.legend(["train error", "test error"], loc="upper right")
-    pylab.grid(True, linestyle='-', color='0.75')
-    pylab.show()
 
 df, X, Y, median_ages = load_train_data()
 
--- titanic/utils.py
+++ titanic/utils.py
@@ -3,6 +3,7 @@
 
 import pandas as pan
 import numpy as np
+from matplotlib import pylab
 
 def load_train_data():
 	#PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
@@ -25,3 +26,15 @@ def load_train_data():
 	X = X.drop(['Sex','Age'], axis=1)
 
 	return df, X, Y, median_ages
+
+def plot_bias_variance(data_sizes, train_errors, test_errors, title):
+    pylab.figure(num=None, figsize=(6, 5))
+    pylab.ylim([0.0, 1.0])
+    pylab.xlabel('Data set size')
+    pylab.ylabel('Error')
+    pylab.title(title)
+    pylab.plot(
+        data_sizes, test_errors, "--", data_sizes, train_errors, "b-", lw=1)
+    pylab.legend(["train error", "test error"], loc="upper right")
+    pylab.grid(True, linestyle='-', color='0.75')
+    pylab.show()
