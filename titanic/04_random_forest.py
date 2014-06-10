56cc72ca958c4ad9fbccb659170d9a4e35c8136c
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2014-06-10 14:39:41 +0200

    separate enriched features from basic decision tree exploration + learning curve for tree

--- titanic/04_random_forest.py
+++ titanic/04_random_forest.py
@@ -5,101 +5,73 @@ import csv, sys
 import numpy as np
 import scipy as sp
 import pandas as pan
-from sklearn.cross_validation import KFold, train_test_split
+from sklearn.cross_validation import KFold
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from matplotlib import pylab
-from utils import load_train_data, add_sex_bit, fill_fare, output_predictions, fill_median_age
+from utils import load_train_data, add_sex_bit, fill_fare, fill_median_age, output_predictions, plot_learning_curve
+
+formatting_functions = [add_sex_bit, fill_fare, fill_median_age]
+features = ['Pclass', 'SibSp', 'Parch', 'Fare', 'SexBit', 'AgeFill']
 
 best_score = 0.0
-best_classifier = None
-best_classifier_name = None
+best_classifier = ()
 
-def test_algo(algo, features, classes, name, options={}):
-	cv = KFold(n=len(features), n_folds=8, indices=True)
+def test_algo(algo, data, classes, name, options={}):
+	cv = KFold(n=len(data), n_folds=8, indices=True)
 
 	scores = []
 	for train, test in cv:
-		X_train, y_train = features.values[train], classes.values[train]
-		X_test, y_test = features.values[test], classes.values[test]
+		X_train, y_train = data.values[train], classes.values[train]
+		X_test, y_test = data.values[test], classes.values[test]
 
-		classifier = algo(**options)
+		classifier = algo(random_state = 0, **options)
 		classifier.fit(X_train, y_train)
 
 		scores.append(classifier.score(X_test, y_test))
 
 	score = np.mean(scores)
-	print "Score on training set (with cross-validation) for %s : %.4f" % (name, score)
-	with open('decisiontree.dot', 'w') as f:
-		f = export_graphviz(classifier, out_file = f, feature_names = X.columns.tolist())
+	print "Score on training set (with cross-validation) for %s : %.5f" % (name, score)
+	# with open('decisiontree.dot', 'w') as f:
+	# 	f = export_graphviz(classifier, out_file = f, feature_names = X.columns.tolist())
 	global best_score, best_classifier, best_classifier_name
 	if score >= best_score:
 		best_score = score
-		best_classifier = classifier
-		best_classifier_name = name
+		best_classifier = (classifier, algo, options, name)
 
-df, X, Y = load_train_data()
-X = X[['Pclass', 'SibSp', 'Parch', 'Fare', 'SexBit', 'AgeFill']]
+df, X, Y = load_train_data(formatting_functions)
+X = X[features]
 
-#test_algo(DecisionTreeClassifier, X, Y, "Decision Tree")
-# test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with entropy criterion", {'criterion': 'entropy'})
-# test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with all features", {'max_features': None})
-test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with max_depth=5", {'max_depth': 3})
-# test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with min_samples_leaf=5", {'min_samples_leaf': 5})
+test_algo(DecisionTreeClassifier, X, Y, "Decision Tree")
+test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with entropy criterion", {'criterion': 'entropy'})
+test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with sqrt(features)", {'max_features': 'sqrt'})
+test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with max_depth=5", {'max_depth': 5})
+test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with min_samples_leaf=5", {'min_samples_leaf': 5})
 
-# print
-# for min_samples in range(1,20):
-# 	test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with max_depth=10 and min_samples_leaf=%d" % min_samples, {'max_depth': 10, 
-# 																													  'min_samples_leaf': min_samples})
-# print
-# # min_samples_leaf = 8 is the best
-# for max_depth in range(1,20):
-# 	test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with max_depth=%d and min_samples_leaf=8" % max_depth, {'max_depth': max_depth, 
-# 																												   'min_samples_leaf': 8})
-# # max_depth = 10 seems to stay the best
-# print
+print
+for min_samples in range(1,21):
+	for max_depth in range(1,21):
+		test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with max_depth=%d and min_samples_leaf=%d" % 
+			(max_depth, min_samples), {'max_depth': max_depth, 'min_samples_leaf': min_samples})
+print
 
-# test_algo(RandomForestClassifier, X, Y, "Random Forest")
-# test_algo(RandomForestClassifier, X, Y, "Random Forest whose trees have max_depth=10 and min_samples_leaf=8", 
-# 			{'max_depth': 10, 'min_samples_leaf': 8})
-# test_algo(RandomForestClassifier, X, Y, "Random Forest with 50 trees", {'n_estimators': 50})
-# test_algo(RandomForestClassifier, X, Y, "Random Forest with 100 trees", {'n_estimators': 100})
-# test_algo(RandomForestClassifier, X, Y, "Random Forest with 100 trees and the best params for trees", 
-# 			{'n_estimators': 100, 'max_depth': 10, 'min_samples_leaf': 8})
-# print
+test_algo(RandomForestClassifier, X, Y, "Random Forest")
+test_algo(RandomForestClassifier, X, Y, "Random Forest whose trees have max_depth=10 and min_samples_leaf=8", 
+			{'max_depth': 10, 'min_samples_leaf': 8})
+test_algo(RandomForestClassifier, X, Y, "Random Forest with 50 trees", {'n_estimators': 50})
+test_algo(RandomForestClassifier, X, Y, "Random Forest with 100 trees", {'n_estimators': 100})
+test_algo(RandomForestClassifier, X, Y, "Random Forest with 100 trees and the best params for trees", 
+			{'n_estimators': 100, 'max_depth': 10, 'min_samples_leaf': 8})
+print
 
-# def extract_title(s):
-# 	if 'Mr.' in s: 
-# 		return 'Mr.'
-# 	if 'Mrs.' in s:
-# 		return 'Mrs.'
-# 	if 'Miss.' in s:
-# 		return 'Miss'
-# 	if 'Rev.' in s:
-# 		return 'Rev.'
-# 	if 'Major.' in s:
-# 		return 'Major.'
-# 	if 'Lady.' in s:
-# 		return 'Lady.'
-# 	return 'Other'
 
-# embarked_encoder = LabelEncoder()
-# X['Embarked'] = embarked_encoder.fit_transform(df['Embarked'])
-# ticket_encoder = LabelEncoder()
-# X['Ticket'] = ticket_encoder.fit_transform(df['Ticket'].str[0])
-# title_encoder = LabelEncoder()
-# X['Title'] = title_encoder.fit_transform(df['Name'].apply(lambda name: extract_title(name)))
+print '='*100
+print
 
-# test_algo(DecisionTreeClassifier, X, Y, "Decision Tree with best params and Embarked, Ticket and Title", {'max_depth': 10, 'min_samples_leaf': 8})
-# test_algo(RandomForestClassifier, X, Y, "Random Forest with best params, 100 trees, and Embarked, Ticket and Title", 
-# 			{'n_estimators': 100, 'max_depth': 10, 'min_samples_leaf': 8})
+classifier, algo, options, name = best_classifier
+print "Best overall: %s with %.5f" % (name, best_score)
 
-# print
-# print '='*100
-# print
+output_predictions(classifier, '04_submission.csv', formatting_functions, features)
 
-# print "Best overall: %s with %.4f" % (best_classifier_name, best_score)
-
-# output_predictions(best_classifier, '04_submission.csv', ['Pclass', 'SexBit', 'AgeFill', 'SibSp', 'Parch', 'Fare'],
-# 				   [add_sex_bit, fill_fare, fill_median_age])
+plot_learning_curve(name, algo, options, X, Y, min_size=50, n_steps=50)
--- titanic/utils.py
+++ titanic/utils.py
@@ -5,7 +5,7 @@ import pandas as pan
 import numpy as np
 import re
 from matplotlib import pylab
-from sklearn.cross_validation import KFold
+from sklearn.cross_validation import KFold, train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 def add_sex_bit(X):
@@ -25,6 +25,14 @@ def fill_median_age(X):
 		for j in range(3):
 			X.loc[(X.Age.isnull()) & (X.SexBit == i) & (X.Pclass-1 == j), 'AgeFill'] = median_ages[i,j]
 
+def encode_embarked(X):
+	embarked_encoder = LabelEncoder()
+	X['Embarked'] = embarked_encoder.fit_transform(X['Embarked'])
+
+def extract_deck(X):
+	deck_encoder = LabelEncoder()
+	X['Deck'] = deck_encoder.fit_transform(X['Ticket'].str[0])
+
 def add_title(X, names=None):
 	pattern = re.compile(r'.*(Mr|Mrs|Miss|Rev|Major|Col|Lady|Master|Dr).*')
 	def extract_title(name):
@@ -38,16 +46,16 @@ def add_title(X, names=None):
 	title_encoder = LabelEncoder()
 	X['Title'] = title_encoder.fit_transform(names.apply(extract_title))
 
-def load_train_data():
+def load_train_data(format_funcs = []):
 	#PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 	df = pan.read_csv('train.csv', header=0)
 
 	Y = df['Survived']
 
 	X = df.drop(['Survived'], axis=1)
-	add_sex_bit(X)
-	fill_fare(X)
-	fill_median_age(X)
+
+	for func in format_funcs:
+		func(X)
 
 	return df, X, Y
 
@@ -67,19 +75,35 @@ def test_algo(algo, features, classes, name, options={}):
 	score = np.mean(scores)
 	print "Score on training set (with cross-validation) for %s : %.4f" % (name, score)
 
-def plot_bias_variance(data_sizes, train_errors, test_errors, title):
+def plot_bias_variance(datasizes, train_errors, test_errors, title):
     pylab.figure(num=None, figsize=(6, 5))
     pylab.ylim([0.0, 1.0])
-    pylab.xlabel('Data set size')
+    pylab.xlabel('Training set size')
     pylab.ylabel('Error')
     pylab.title(title)
     pylab.plot(
-        data_sizes, test_errors, "--", data_sizes, train_errors, "b-", lw=1)
+        datasizes, test_errors, "--", datasizes, train_errors, "b-", lw=1)
     pylab.legend(["train error", "test error"], loc="upper right")
     pylab.grid(True, linestyle='-', color='0.75')
     pylab.show()
 
-def output_predictions(classifier, output_name, features=[], format_funcs = []):
+def plot_learning_curve(name, algo, options, X, Y, min_size=50, n_steps=50, test_size=0.3):
+	datasizes = np.arange(min_size, len(X), len(X)/n_steps)
+	if datasizes[-1] != len(X):
+		datasizes[-1] = len(X)
+
+	train_errors = []
+	cv_errors = []
+	for datasize in datasizes:
+		X_train, X_cv, y_train, y_cv = train_test_split(X[:datasize], Y[:datasize], test_size=test_size)
+		classifier = algo(**options)
+		classifier.fit(X_train, y_train)
+		train_errors.append(1 - classifier.score(X_train, y_train))
+		cv_errors.append(1 - classifier.score(X_cv, y_cv))
+
+	plot_bias_variance(datasizes, train_errors, cv_errors, "Learning curve for\n%s" % name)
+
+def output_predictions(classifier, output_name, format_funcs = [], features=[]):
 	Xtest = pan.read_csv('test.csv', header=0)
 	
 	ids = Xtest['PassengerId'].values
