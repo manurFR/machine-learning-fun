0b723e48bbd4dfacdd96c0d16f2bf865d1d482d2
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2014-06-16 18:23:46 +0200

    move best but not reproductible submission and code to 99

---
+++ titanic/99_title_and_log_fare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+import numpy as np
+from utils import load_train_data, output_predictions, add_sex_bit, fill_fare, add_title, test_algo
+from sklearn.tree import DecisionTreeClassifier
+
+def logarize_fare(X):
+	X['FareLog'] = np.log10(X['Fare'] + 1)
+
+df, X, Y = load_train_data([add_sex_bit, fill_fare, add_title, logarize_fare])
+
+features = ['Pclass', 'SibSp', 'Parch', 'FareLog', 'SexBit', 'Title']
+
+test_algo(DecisionTreeClassifier, X[features], Y, "Decision Tree with Title instead of Age and log(Fare+1)", {'max_depth': 10, 'min_samples_leaf': 8})
+
+classifier = DecisionTreeClassifier(max_depth = 10, min_samples_leaf = 8)
+classifier.fit(X[features], Y)
+
+output_predictions(classifier, "06_submission.csv", [add_sex_bit, fill_fare, add_title, logarize_fare], features)
+
+print
+print "Importance of features:"
+print features
+print classifier.feature_importances_
