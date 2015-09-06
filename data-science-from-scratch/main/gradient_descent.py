43364175074e943730fc895d5081a7a581a03640
Emmanuel Bizieau <manur@manur.org>
2015-09-06 14:08:27 +0200

    [dsfs] copy-paste stochastic gradient descent

--- data-science-from-scratch/main/gradient_descent.py
+++ data-science-from-scratch/main/gradient_descent.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 import random
-from linalg import distance
+from linalg import distance, scalar_multiply, vector_substract
 
 
 def difference_quotient(function, x, delta):
@@ -57,24 +57,6 @@ def minimize_batch(target_func, gradient_func, theta_0, tolerance=0.000001):
         else:
             theta, value = next_theta, next_value
 
-if __name__ == '__main__':
-    # gradient descent de la fonction 'carré' -- la dérivée de f(x)=x**2 est f'(x)=2x
-    # Comme on peut la définir mathématiquement, on utilise directement cette dérivée dans l'algorithme, au lieu
-    # de l'estimer par estimate_gradient() qui est très couteux en nb d'appels à la fonction que l'on cherche
-    # à optimiser.
-    def gradient_of_square(coordinates):
-        return [2 * c for c in coordinates]
-
-    point = [random.randint(-10, 10) for _ in range(3)]  # random starting point
-    tolerance = 0.0000001
-    while True:
-        gradient = gradient_of_square(point)
-        next_point = step(point, gradient, -0.01)  # step négative pour minimiser la fonction et chercher le minimum
-        if distance(next_point, point) < tolerance:
-            break
-        point = next_point
-    print "Minimum trouvé :", point
-
 
 def negate(function):
     return lambda *args, **kwargs: -function(*args, **kwargs)
@@ -97,3 +79,59 @@ def in_random_order(data):
     random.shuffle(indexes)
     for i in indexes:
         yield data[i]
+
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+
+    data = zip(x, y)
+    theta = theta_0                             # initial guess
+    alpha = alpha_0                             # initial step size
+    min_theta, min_value = None, float("inf")   # the minimum so far
+    iterations_with_no_improvement = 0
+
+    # if we ever go 100 iterations with no improvement, stop
+    while iterations_with_no_improvement < 100:
+        value = sum( target_fn(x_i, y_i, theta) for x_i, y_i in data )
+
+        if value < min_value:
+            # if we've found a new minimum, remember it
+            # and go back to the original step size
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            # otherwise we're not improving, so try shrinking the step size
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+
+        # and take a gradient step for each of the data points
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_substract(theta, scalar_multiply(alpha, gradient_i))
+
+    return min_theta
+
+
+def maximize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    return minimize_stochastic(negate(target_fn),
+                               negate_all(gradient_fn),
+                               x, y, theta_0, alpha_0)
+
+if __name__ == '__main__':
+    # gradient descent de la fonction 'carré' -- la dérivée de f(x)=x**2 est f'(x)=2x
+    # Comme on peut la définir mathématiquement, on utilise directement cette dérivée dans l'algorithme, au lieu
+    # de l'estimer par estimate_gradient() qui est très couteux en nb d'appels à la fonction que l'on cherche
+    # à optimiser.
+    def gradient_of_square(coordinates):
+        return [2 * c for c in coordinates]
+
+    point = [random.randint(-10, 10) for _ in range(3)]  # random starting point
+    tolerance = 0.0000001
+    while True:
+        gradient = gradient_of_square(point)
+        next_point = step(point, gradient, -0.01)  # step négative pour minimiser la fonction et chercher le minimum
+        if distance(next_point, point) < tolerance:
+            break
+        point = next_point
+    print "Minimum trouvé :", point
+
