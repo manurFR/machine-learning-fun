4e0f8fa263b111caad5d426169a6ed1e2aaf68d4
Emmanuel Bizieau <manur@manur.org>
2015-09-15 17:46:45 +0200

    [dsfs] working with data: more complicated grouping

--- data-science-from-scratch/main/working_with_data.py
+++ data-science-from-scratch/main/working_with_data.py
@@ -91,64 +91,64 @@ def group_by(grouper_func, rows, value_transform=None):
         return {key: value_transform(rows) for key, rows in grouped_dict.iteritems()}
 
 if __name__ == '__main__':
-    def random_normal():
-        return inverse_normal_cdf(random.random())
-
-    random.seed(0)
-
-    uniform = [200 * random.random() - 100 for _ in range(20000)]
-    normal = [54 * random_normal() for _ in range(20000)]
-
-    plot_histogram(uniform, 10, "Uniform histogram")
-    plot_histogram(normal, 10, "Normal histogram")
-
-    # two dimensions
-    xs = [random_normal() for _ in range(1000)]
-    ys1 = [x + random_normal() / 2 for x in xs]
-    ys2 = [-x + random_normal() / 2 for x in xs]
-
-    plot_histogram(ys1, 0.5, "ys1")
-    plot_histogram(ys2, 0.5, "ys2")
-
-    plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
-    plt.scatter(xs, ys2, marker='.', color='red', label='ys2')
-    plt.legend(loc=9)
-    plt.title("Very different joint distributions")
-    plt.show()
-
-    print correlation(xs, ys1), correlation(xs, ys2)
-
-    # scatterplot matrix
-    # prepare data
-    def make_row():
-        v0 = random_normal()
-        v1 = -5 * v0 + random_normal()  # negatively correlated to v0
-        v2 = v0 + v1 + 5 * random_normal()  # positively correlated to both v0 and v1
-        v3 = 6 if v2 > -2 else 0  # depends exclusively on v2
-        return [v0, v1, v2, v3]
-    data = [make_row() for _ in range(100)]
-
-    # plot it
-    _, num_columns = shape(data)
-    fig, ax = plt.subplots(num_columns, num_columns)
-
-    for i in range(num_columns):
-        for j in range(num_columns):
-            if i != j:
-                ax[i][j].scatter(get_column(data, j), get_column(data, i))
-            else:
-                ax[i][j].annotate("series " + str(i), (0.5, 0.5), xycoords='axes fraction', ha='center', va='center')
-
-            # hide axis labels except for left and bottom charts
-            if i < num_columns - 1:
-                ax[i][j].xaxis.set_visible(False)
-            if j > 0:
-                ax[i][j].yaxis.set_visible(False)
-
-    ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
-    ax[0][0].set_ylim(ax[0][1].get_ylim())
-
-    plt.show()
+    # def random_normal():
+    #     return inverse_normal_cdf(random.random())
+    #
+    # random.seed(0)
+    #
+    # uniform = [200 * random.random() - 100 for _ in range(20000)]
+    # normal = [54 * random_normal() for _ in range(20000)]
+    #
+    # plot_histogram(uniform, 10, "Uniform histogram")
+    # plot_histogram(normal, 10, "Normal histogram")
+    #
+    # # two dimensions
+    # xs = [random_normal() for _ in range(1000)]
+    # ys1 = [x + random_normal() / 2 for x in xs]
+    # ys2 = [-x + random_normal() / 2 for x in xs]
+    #
+    # plot_histogram(ys1, 0.5, "ys1")
+    # plot_histogram(ys2, 0.5, "ys2")
+    #
+    # plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
+    # plt.scatter(xs, ys2, marker='.', color='red', label='ys2')
+    # plt.legend(loc=9)
+    # plt.title("Very different joint distributions")
+    # plt.show()
+    #
+    # print correlation(xs, ys1), correlation(xs, ys2)
+    #
+    # # scatterplot matrix
+    # # prepare data
+    # def make_row():
+    #     v0 = random_normal()
+    #     v1 = -5 * v0 + random_normal()  # negatively correlated to v0
+    #     v2 = v0 + v1 + 5 * random_normal()  # positively correlated to both v0 and v1
+    #     v3 = 6 if v2 > -2 else 0  # depends exclusively on v2
+    #     return [v0, v1, v2, v3]
+    # data = [make_row() for _ in range(100)]
+    #
+    # # plot it
+    # _, num_columns = shape(data)
+    # fig, ax = plt.subplots(num_columns, num_columns)
+    #
+    # for i in range(num_columns):
+    #     for j in range(num_columns):
+    #         if i != j:
+    #             ax[i][j].scatter(get_column(data, j), get_column(data, i))
+    #         else:
+    #             ax[i][j].annotate("series " + str(i), (0.5, 0.5), xycoords='axes fraction', ha='center', va='center')
+    #
+    #         # hide axis labels except for left and bottom charts
+    #         if i < num_columns - 1:
+    #             ax[i][j].xaxis.set_visible(False)
+    #         if j > 0:
+    #             ax[i][j].yaxis.set_visible(False)
+    #
+    # ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
+    # ax[0][0].set_ylim(ax[0][1].get_ylim())
+    #
+    # plt.show()
 
     # parsing data
     data = []
@@ -161,3 +161,40 @@ if __name__ == '__main__':
     for row in data:
         if any(x is None for x in row):
             print row
+
+    dictdata = [{'date': row[0], 'symbol': row[1], 'closing_price': row[2]} for row in data]
+
+    max_price_by_symbol = group_by(picker("symbol"),
+                                   dictdata,
+                                   lambda rows: max(pluck("closing_price", rows)))
+    print max_price_by_symbol
+
+    # more complicated grouping
+    fullstocks = "https://raw.githubusercontent.com/joelgrus/data-science-from-scratch/master/code/stocks.txt"
+    reader = csv.DictReader(StringIO(requests.get(fullstocks).text), delimiter="\t")
+    stocksdata = [parse_dict(row, {'date': dateutil.parser.parse, 'closing_price': float}) for row in reader]
+
+    def percent_price_change(yesterday, today):
+        return today["closing_price"] / yesterday["closing_price"] - 1
+
+    def day_over_day_changes(grouped_rows):
+        ordered = sorted(grouped_rows, key=picker("date"))
+
+        return [{"symbol": today["symbol"], "date": today["date"], "change": percent_price_change(yesterday, today)}
+                for yesterday, today in zip(ordered, ordered[1:])]
+
+    changes_by_symbol = group_by(picker("symbol"), stocksdata, day_over_day_changes)
+
+    all_changes = [change for changes in changes_by_symbol.values() for change in changes]
+
+    print max(all_changes, key=picker("change")), min(all_changes, key=picker("change"))
+
+    def combine_pct_changes(pct_change1, pct_change2):
+        """computes the cumulative effect of two percentage changes, eg +10% -20% yields -12% (and not -10%)"""
+        return (1 + pct_change1) * (1 + pct_change2) - 1
+
+    def overall_changes(changes):
+        return reduce(combine_pct_changes, pluck("change", changes))
+
+    overall_change_by_month = group_by(lambda row: row['date'].month, all_changes, overall_changes)
+    print overall_change_by_month
